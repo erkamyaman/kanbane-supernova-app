e935550a8d17fe9807709d5824662202c968afdc
Kam <erkamyaman35@gmail.com>
2025-08-17 00:39:57 +0300

    Add routerLink to Backlog item in topbar and include RouterLink in imports

--- kanbane-supernova-fe/src/app/layout/topbar/topbar.ts
+++ kanbane-supernova-fe/src/app/layout/topbar/topbar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { PopoverModule } from 'primeng/popover';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
   selector: 'app-topbar',
-  imports: [PopoverModule, ButtonModule, ToggleSwitchModule],
+  imports: [PopoverModule, ButtonModule, ToggleSwitchModule, RouterLink],
   templateUrl: './topbar.html',
   styleUrl: './topbar.scss'
 })
